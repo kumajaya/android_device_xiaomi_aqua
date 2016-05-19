9245d9f2401f4359ac42ec4ab5291121d2c7b0ad
Ketut Putu Kumajaya <ketut.kumajaya@gmail.com>
2016-05-20 00:56:31 +0700

    aqua: Add TWRP support
    Change-Id: Ie67833b48c140b257260de4dc7d35cf653cfd64a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,4 +21,14 @@ TARGET_RECOVERY_FSTAB := device/xiaomi/aqua/fstab.libra
 BOARD_SEPOLICY_DIRS += \
     device/xiaomi/aqua/sepolicy
 
+# TWRP
+RECOVERY_VARIANT := twrp
+TW_THEME := portrait_hdpi
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+RECOVERY_SDCARD_ON_DATA := true
+TW_INCLUDE_CRYPTO := true
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+
 -include vendor/xiaomi/libra/BoardConfigVendor.mk
